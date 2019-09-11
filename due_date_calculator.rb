bca4bad069612cc63c0dcd7c10772a81a832f288
pkrisz0 <p.krisz89@gmail.com>
2019-09-11 14:40:04 +0200

    minor refactoring

--- due_date_calculator.rb
+++ due_date_calculator.rb
@@ -3,11 +3,12 @@ require 'date'
 require 'active_support/time'
 require 'byebug'
 
-class InvalidParamError < StandardError; end
+class InvalidParamsError < StandardError; end
 
 class DueDateCalculator
   def calculate_due_date(submit_date, turnaround)
-    return unless params_approved?(submit_date, turnaround)
+    raise InvalidParamsError unless params_approved?(submit_date, turnaround)
+
     resolution_date(submit_date, turnaround)
   end
 
@@ -16,28 +17,36 @@ class DueDateCalculator
     @hours = turnaround % 8
 
     @hours += 16 unless within_working_hours?(submit_date + @hours.hours)
-    @days += 2 if (submit_date.wday + @days) > 5 || submit_date.wday == 5
+    @days += 2 if turnaround_includes_weekend?(submit_date.wday + @days) || friday?(submit_date)
 
     submit_date + @hours.hour + @days.days
   end
 
   def params_approved?(submit_date, turnaround)
-    params_type_check(submit_date, turnaround) &&
+    params_types_match?(submit_date, turnaround) &&
     weekday?(submit_date) &&
     within_working_hours?(submit_date)
   end
 
-  def params_type_check(submit_date, turnaround)
-    submit_date.is_a?(DateTime) && !turnaround.nil? && turnaround > 0 && turnaround.is_a?(Integer) ? true : raise(InvalidParamError)
+  def params_types_match?(submit_date, turnaround)
+    submit_date.is_a?(DateTime) && !turnaround.nil? && turnaround.positive? && turnaround.is_a?(Integer)
   end
 
   def weekday?(date)
     date.wday < 6
   end
 
+  def friday?(date)
+    date.wday == 5
+  end
+
+  def turnaround_includes_weekend?(day_count)
+     day_count > 5
+  end
+
   def within_working_hours?(date)
     after_or_at_nine = date.hour >= 9
-    after_or_at_nine && (date.hour < 17 || (date.second == 0 && date.minute == 0 && date.hour == 17))
+    after_or_at_nine && (date.hour < 17 || (date.second.zero? && date.minute.zero? && date.hour == 17))
   end
 end
 
@@ -49,32 +58,32 @@ describe DueDateCalculator do
     submit_date = '2019-09-10 13:00:00'.to_datetime
 
     it 'is a submit date and turnaround in hours' do
-      expect { subject.calculate_due_date(submit_date, nil) }.to raise_error InvalidParamError
-      expect { subject.calculate_due_date(nil, turnaround) }.to raise_error InvalidParamError
+      expect { subject.calculate_due_date(submit_date, nil) }.to raise_error InvalidParamsError
+      expect { subject.calculate_due_date(nil, turnaround) }.to raise_error InvalidParamsError
     end
 
     it 'is in a date and an integer format' do
-      expect { subject.calculate_due_date(submit_date.to_s, turnaround) }.to raise_error InvalidParamError
-      expect { subject.calculate_due_date(submit_date, turnaround.to_f) }.to raise_error InvalidParamError
+      expect { subject.calculate_due_date(submit_date.to_s, turnaround) }.to raise_error InvalidParamsError
+      expect { subject.calculate_due_date(submit_date, turnaround.to_f) }.to raise_error InvalidParamsError
     end
   end
 
   context 'submission date' do
-    weekday = '2019-09-10 15:00:00'.to_datetime
-    weekend = '2019-09-14 16:00:00'.to_datetime
+    tuesday = '2019-09-10 15:00:00'.to_datetime
+    weekend = tuesday + 4.days
 
     it 'is a weekday' do
-      expect(subject.calculate_due_date(weekend, turnaround)).to be nil
-      expect(subject.calculate_due_date(weekday, turnaround)).to be_a DateTime
+      expect { subject.calculate_due_date(weekend, turnaround) }.to raise_error InvalidParamsError
+      expect(subject.calculate_due_date(tuesday, turnaround)).to be_a DateTime
     end
 
-    too_soon = '2019-09-10 08:00:00'.to_datetime
-    too_late = '2019-09-11 17:01:00'.to_datetime
     in_time = '2019-09-12 17:00:00'.to_datetime
+    too_soon = in_time - 9.hours
+    too_late = in_time + 1.minute
 
     it 'is between 9AM and 5PM' do
-      expect(subject.calculate_due_date(too_soon, turnaround))
-      expect(subject.calculate_due_date(too_late, turnaround))
+      expect { subject.calculate_due_date(too_soon, turnaround) }.to raise_error InvalidParamsError
+      expect { subject.calculate_due_date(too_late, turnaround) }.to raise_error InvalidParamsError
       expect(subject.calculate_due_date(in_time, turnaround)).to be_a DateTime
     end
   end
@@ -92,14 +101,14 @@ describe DueDateCalculator do
       expect(subject.calculate_due_date(wednesday, 6)).to eq expected_resolution
     end
 
-    it 'friday last working hour should return monday' do
+    it 'returns first day next week if submitted on friday during last working hour' do
       expected_resolution = '2019-09-16 9:58:00'.to_datetime
       expect(subject.calculate_due_date('2019-09-13 16:58:00'.to_datetime, 1)).to eq expected_resolution
     end
 
-    it 'friday last working hour should return tuesday if turnaround is over a working day' do
-      expected_resolution = '2019-09-17 9:58:00'.to_datetime
-      expect(subject.calculate_due_date('2019-09-13 16:58:00'.to_datetime, 9)).to eq expected_resolution
+    it 'returns second day of next week if submitted during last day of week and takes over a working day to resolve' do
+      expected_resolution = '2019-09-17 11:58:00'.to_datetime
+      expect(subject.calculate_due_date('2019-09-13 16:58:00'.to_datetime, 11)).to eq expected_resolution
     end
   end
 end
